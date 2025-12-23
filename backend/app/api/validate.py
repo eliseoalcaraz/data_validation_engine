50e47e7406a250fd730455983977b6b85a7aa228
Eliseo Alcaraz <elipalcaraz@gmail.com>
2025-12-23 10:34:09 +0800

    fix: wrong directory and include the clean data for validation testing

--- backend/app/api/validate.py
+++ backend/app/api/validate.py
@@ -1,6 +1,13 @@
 import pandas as pd
-from  core.validator import validate_csv
+from core.validator import validate_csv
 
-df = pd.read_csv("../test/data/test_data_dirty.csv")
-result = validate_csv(df)
-print(result)
+df_clean = pd.read_csv("../../test/data/test_data_clean.csv")
+df_dirty = pd.read_csv("../../test/data/test_data_dirty.csv")
+
+result_clean = validate_csv(df_clean)
+print("=== CLEAN DATA ===")
+print(result_clean)
+
+result_dirty = validate_csv(df_dirty)
+print("\n=== DIRTY DATA ===")
+print(result_dirty)
